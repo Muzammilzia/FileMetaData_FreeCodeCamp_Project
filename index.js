11f1f242755bf88597f447c7a268240099d6a15e
Muzammilzia <muzzammilzia20@mail.com>
2023-01-31 05:19:37 +0500

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer().any();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(upload);
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', (req, res) => {
+  res.json({
+    name: req.files[0].originalname,
+    type: req.files[0].mimetype,
+    size: req.files[0].size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
